abbe1df140980eefa6bef2135b6f120907442bbb
ravindrajangid00007 <ravindrajangid00007@gmail.com>
2021-05-21 19:31:11 +0530

    Calculator

---
+++ text.js
@@ -0,0 +1,471 @@
+var wh = document.getElementsByClassName('white');
+
+var display = document.getElementById('display');
+
+var re = document.getElementsByClassName('red');
+
+// for (var i = 0; i < 14; i++) {
+    wh[0].addEventListener('mouseenter', function () {
+        wh[0].style.backgroundColor = 'gray';
+    });
+
+    wh[0].addEventListener('mouseleave', function () {
+        wh[0].style.backgroundColor = 'white';
+    });
+
+
+    wh[0].addEventListener('mousedown', function () {
+        wh[0].style.boxShadow = "1px 1px 1px 1px  gray" ;
+    });
+
+
+    wh[0].addEventListener('mouseup', function () {
+        wh[0].style.boxShadow = "0px 0px 0px 0px  white" ;
+    });
+
+    wh[0].addEventListener('click' , function(){
+        
+    });
+// }
+
+// for(var i in wh){
+//     console.log(i);
+// }
+
+
+wh[1].addEventListener('mouseenter', function (){
+    wh[1].style.backgroundColor = 'gray';
+});
+
+wh[1].addEventListener('mouseleave', function (){
+    wh[1].style.backgroundColor = 'white';
+});
+
+
+wh[1].addEventListener('mousedown', function(){ 
+    wh[1].style.boxShadow = "1px 1px 1px 1px  gray" 
+});
+
+
+wh[1].addEventListener('mouseup', function(){ wh[1].style.boxShadow = "0px 0px 0px 0px  white" 
+});
+
+wh[1].addEventListener('click' , function(){
+    
+});
+
+
+
+wh[2].addEventListener('mouseenter', function (){
+    wh[2].style.backgroundColor = 'gray';
+});
+
+wh[2].addEventListener('mouseleave', function (){
+    wh[2].style.backgroundColor = 'white';
+});
+
+
+wh[2].addEventListener('mousedown', function(){ 
+    wh[2].style.boxShadow = "1px 1px 1px 1px  gray" 
+});
+
+
+wh[2].addEventListener('mouseup', function(){ 
+    wh[2].style.boxShadow = "0px 0px 0px 0px  white" 
+});
+
+wh[2].addEventListener('click' , function(){
+    
+});
+//3
+
+
+wh[3].addEventListener('mouseenter', function (){
+    wh[3].style.backgroundColor = 'gray';
+});
+
+wh[3].addEventListener('mouseleave', function (){
+    wh[3].style.backgroundColor = 'white';
+});
+
+
+wh[3].addEventListener('mousedown', function(){ 
+    wh[3].style.boxShadow = "1px 1px 1px 1px  gray" 
+});
+
+
+wh[3].addEventListener('mouseup', function(){ 
+    wh[3].style.boxShadow = "0px 0px 0px 0px  white" 
+});
+wh[3].addEventListener('click' , function(){
+    
+});
+//4
+
+wh[4].addEventListener('mouseenter', function (){
+    wh[4].style.backgroundColor = 'gray';
+});
+
+wh[4].addEventListener('mouseleave', function (){
+    wh[4].style.backgroundColor = 'white';
+});
+
+
+wh[4].addEventListener('mousedown', function(){ 
+    wh[4].style.boxShadow = "1px 1px 1px 1px  gray" 
+});
+
+
+wh[4].addEventListener('mouseup', function(){ 
+    wh[4].style.boxShadow = "0px 0px 0px 0px  white" 
+});
+
+wh[4].addEventListener('click' , function(){
+   
+});
+
+//5
+wh[5].addEventListener('mouseenter', function (){
+    wh[5].style.backgroundColor = 'gray';
+});
+
+wh[5].addEventListener('mouseleave', function (){
+    wh[5].style.backgroundColor = 'white';
+});
+
+
+wh[5].addEventListener('mousedown', function(){ 
+    wh[5].style.boxShadow = "1px 1px 1px 1px  gray" 
+});
+
+
+wh[5].addEventListener('mouseup', function(){ 
+    wh[5].style.boxShadow = "0px 0px 0px 0px  white" 
+});
+
+//6
+wh[6].addEventListener('mouseenter', function (){
+    wh[6].style.backgroundColor = 'gray';
+});
+
+wh[6].addEventListener('mouseleave', function (){
+    wh[6].style.backgroundColor = 'white';
+});
+
+
+wh[6].addEventListener('mousedown', function(){ 
+    wh[6].style.boxShadow = "1px 1px 1px 1px  gray" 
+});
+
+
+wh[6].addEventListener('mouseup', function(){ 
+    wh[6].style.boxShadow = "0px 0px 0px 0px  white" 
+});
+
+//7
+wh[7].addEventListener('mouseenter', function (){
+    wh[7].style.backgroundColor = 'gray';
+});
+
+wh[7].addEventListener('mouseleave', function (){
+    wh[7].style.backgroundColor = 'white';
+});
+
+
+wh[7].addEventListener('mousedown', function(){ 
+    wh[7].style.boxShadow = "1px 1px 1px 1px  gray" 
+});
+
+
+wh[7].addEventListener('mouseup', function(){ 
+    wh[7].style.boxShadow = "0px 0px 0px 0px  white" 
+});
+
+//8
+wh[8].addEventListener('mouseenter', function (){
+    wh[8].style.backgroundColor = 'gray';
+});
+
+wh[8].addEventListener('mouseleave', function (){
+    wh[8].style.backgroundColor = 'white';
+});
+
+
+wh[8].addEventListener('mousedown', function(){ 
+    wh[8].style.boxShadow = "1px 1px 1px 1px  gray" 
+});
+
+
+wh[8].addEventListener('mouseup', function(){ 
+    wh[8].style.boxShadow = "0px 0px 0px 0px  white" 
+});
+
+//9
+wh[9].addEventListener('mouseenter', function (){
+    wh[9].style.backgroundColor = 'gray';
+});
+
+wh[9].addEventListener('mouseleave', function (){
+    wh[9].style.backgroundColor = 'white';
+});
+
+
+wh[9].addEventListener('mousedown', function(){ 
+    wh[9].style.boxShadow = "1px 1px 1px 1px  gray" 
+});
+
+
+wh[9].addEventListener('mouseup', function(){ 
+    wh[9].style.boxShadow = "0px 0px 0px 0px  white" 
+});
+
+
+//10
+wh[10].addEventListener('mouseenter', function (){
+    wh[10].style.backgroundColor = 'gray';
+});
+
+wh[10].addEventListener('mouseleave', function (){
+    wh[10].style.backgroundColor = 'white';
+});
+
+
+wh[10].addEventListener('mousedown', function(){ 
+    wh[10].style.boxShadow = "1px 1px 1px 1px  gray" 
+});
+
+
+wh[10].addEventListener('mouseup', function(){ 
+    wh[10].style.boxShadow = "0px 0px 0px 0px  white" 
+});
+
+//11
+wh[11].addEventListener('mouseenter', function (){
+    wh[11].style.backgroundColor = 'gray';
+});
+
+wh[11].addEventListener('mouseleave', function (){
+    wh[11].style.backgroundColor = 'white';
+});
+
+
+wh[11].addEventListener('mousedown', function(){ 
+    wh[11].style.boxShadow = "1px 1px 1px 1px  gray" 
+});
+
+
+wh[11].addEventListener('mouseup', function(){ 
+    wh[11].style.boxShadow = "0px 0px 0px 0px  white" 
+});
+
+
+//12
+wh[12].addEventListener('mouseenter', function (){
+    wh[12].style.backgroundColor = 'gray';
+});
+
+wh[12].addEventListener('mouseleave', function (){
+    wh[12].style.backgroundColor = 'white';
+});
+
+
+wh[12].addEventListener('mousedown', function(){ 
+    wh[12].style.boxShadow = "1px 1px 1px 1px  gray" 
+});
+
+
+wh[12].addEventListener('mouseup', function(){ 
+    wh[12].style.boxShadow = "0px 0px 0px 0px  white" 
+});
+
+
+//13
+
+wh[13].addEventListener('mouseenter', function (){
+    wh[13].style.backgroundColor = 'gray';
+});
+
+wh[13].addEventListener('mouseleave', function (){
+    wh[13].style.backgroundColor = 'white';
+});
+
+
+wh[13].addEventListener('mousedown', function(){ 
+    wh[13].style.boxShadow = "1px 1px 1px 1px  gray" 
+});
+
+
+wh[13].addEventListener('mouseup', function(){ 
+    wh[13].style.boxShadow = "0px 0px 0px 0px  white" 
+});
+
+
+//14
+wh[14].addEventListener('mouseenter', function (){
+    wh[14].style.backgroundColor = 'gray';
+});
+
+wh[14].addEventListener('mouseleave', function (){
+    wh[14].style.backgroundColor = 'white';
+});
+
+
+wh[14].addEventListener('mousedown', function(){ 
+    wh[14].style.boxShadow = "1px 1px 1px 1px  gray" 
+});
+
+
+wh[14].addEventListener('mouseup', function(){ 
+    wh[14].style.boxShadow = "0px 0px 0px 0px  white" 
+});
+
+
+//arithmatic
+
+
+
+re[0].addEventListener('mouseenter', function (){
+    re[0].style.backgroundColor = 'orangered';
+});
+
+re[0].addEventListener('mouseleave', function (){
+    re[0].style.backgroundColor = 'rgb(155, 82, 82)';
+});
+
+
+re[0].addEventListener('mousedown', function(){ 
+    re[0].style.boxShadow = "1px 1px 1px 1px  gray" 
+});
+
+
+re[0].addEventListener('mouseup', function(){ 
+    re[0].style.boxShadow = "0px 0px 0px 0px  white" 
+});
+
+//1
+re[1].addEventListener('mouseenter', function (){
+    re[1].style.backgroundColor = 'orangered';
+});
+
+re[1].addEventListener('mouseleave', function (){
+    re[1].style.backgroundColor = 'rgb(155, 82, 82)';
+});
+
+
+re[1].addEventListener('mousedown', function(){ 
+    re[1].style.boxShadow = "1px 1px 1px 1px  gray" 
+});
+
+
+re[1].addEventListener('mouseup', function(){ 
+    re[1].style.boxShadow = "0px 0px 0px 0px  white" 
+});
+
+
+//2
+
+re[2].addEventListener('mouseenter', function (){
+    re[2].style.backgroundColor = 'orangered';
+});
+
+re[2].addEventListener('mouseleave', function (){
+    re[2].style.backgroundColor = 'rgb(155, 82, 82)';
+});
+
+
+re[2].addEventListener('mousedown', function(){ 
+    re[2].style.boxShadow = "1px 1px 1px 1px  gray" 
+});
+
+
+re[2].addEventListener('mouseup', function(){ 
+    re[2].style.boxShadow = "0px 0px 0px 0px  white" 
+});
+
+
+//3
+
+re[3].addEventListener('mouseenter', function (){
+    re[3].style.backgroundColor = 'orangered';
+});
+
+re[3].addEventListener('mouseleave', function (){
+    re[3].style.backgroundColor = 'rgb(155, 82, 82)';
+});
+
+
+re[3].addEventListener('mousedown', function(){ 
+    re[3].style.boxShadow = "1px 1px 1px 1px  gray" 
+});
+
+
+re[3].addEventListener('mouseup', function(){ 
+    re[3].style.boxShadow = "0px 0px 0px 0px  white" 
+});
+
+//4
+
+
+re[4].addEventListener('mouseenter', function (){
+    re[4].style.backgroundColor = 'orangered';
+});
+
+re[4].addEventListener('mouseleave', function (){
+    re[4].style.backgroundColor = 'rgb(155, 82, 82)';
+});
+
+
+re[4].addEventListener('mousedown', function(){ 
+    re[4].style.boxShadow = "1px 1px 1px 1px  gray" 
+});
+
+
+re[4].addEventListener('mouseup', function(){ 
+    re[4].style.boxShadow = "0px 0px 0px 0px  white" 
+});
+
+
+
+
+//input-box
+operand1 = 0;
+operand2 = null;
+operator = null ;
+function isOperator(value){
+    return value == '+' || value == '-' || value == '*' || value == '/' ;
+}
+for(var i = 0 ; i < wh.length ; i++){
+    wh[i].addEventListener('click' , function(){
+        var value = this.firstElementChild.attributes[1].nodeValue ;
+        console.log(value);
+        if(value == 'AC'){
+            display.textContent = '' ;
+        }else if(value == '+/-'){
+            display.textContent *= -1 ;
+        }
+        else{
+            display.textContent += value;
+        }
+    });
+}
+
+for(var j = 0 ; j < re.length ; j++){
+    re[j].addEventListener('click' , function(){
+        var value = this.firstElementChild.attributes[1].nodeValue;
+        if(isOperator(value)){
+            operator = value;
+            operand1 = parseFloat(display.textContent) ;
+            display.textContent += value ;
+        }else if(value == '='){
+            operand2 = parseFloat(display.textContent);
+            var result = eval(operand1 + ' ' + operator + ' ' + operand2);
+            if(result){
+                display.textContent = result;
+                operand1 = result;
+                operand2 = null;
+                operator = null;
+            }
+        }
+    });
+}
+
